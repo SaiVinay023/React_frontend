30d059fd77b0e2813ca4863a57938e84d2809750
SaiVinay023 <saivinay023@gmail.com>
2025-05-05 21:34:42 +0200

    few files

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,23 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { Routes, Route } from "react-router-dom";
+import Dashboard from "./pages/Dashboard";
+import Upload from "./pages/Upload";
+import MediaLibrary from "./pages/MediaLibrary";
+import PatientDirectory from "./pages/PatientDirectory";
+import Login from "./pages/Login";
+import { AuthProvider } from "./auth/AuthContext";
+import PrivateRoute from "./auth/PrivateRoute";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AuthProvider>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/dashboard" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
+        <Route path="/upload" element={<PrivateRoute><Upload /></PrivateRoute>} />
+        <Route path="/media" element={<PrivateRoute><MediaLibrary /></PrivateRoute>} />
+        <Route path="/patients" element={<PrivateRoute><PatientDirectory /></PrivateRoute>} />
+      </Routes>
+    </AuthProvider>
   );
 }
 
---
+++ my-app/src/pages/Home.js
@@ -0,0 +1,4 @@
+export default function Home() {
+    return <div className="text-2xl font-bold text-center">Home Page</div>;
+  }
+  
